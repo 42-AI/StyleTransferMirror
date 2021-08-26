4129c6485920687778d0a03b5dfbaa8ed371149d
quentin feuillade montixi <quentinfeuillademontixi42@gmail.com>
2021-08-26 22:44:54 +0200

    refactor: clean and comment the training

--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
 import torch
+import torch.utils.data as data
+import torchvision
+
+import os
+
+from PIL import Image
+
+import gc
 
 eps = 1e-5
 
@@ -24,3 +32,61 @@ def mean_std(X : torch.Tensor):
 def normalize(X: torch.Tensor):
     _mean, _std = mean_std(X)
     return (X - _mean) / _std
+
+# clear the gpu memory
+def clean():
+    gc.collect()
+    torch.cuda.empty_cache()
+
+# Load batch from a torch dataload indefinitly
+def infinite_loader(dataloader):
+    while True:
+         for batch in dataloader:
+            yield batch
+            
+class FlatFolderDataset(data.Dataset):
+    def __init__(self, _dir : str, size = 512):
+        '''
+        _dir : path to the folder containing the images
+        size : size for inference
+        '''
+
+        super().__init__()
+
+        self.transform = torchvision.transforms.Compose([
+            torchvision.transforms.Resize(size),
+            torchvision.transforms.RandomCrop(size/2),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.ColorJitter(.15,.2,.2),
+            torchvision.transforms.ToTensor(),
+        ])
+
+        # This transformation is used for inference.
+        # Because the network is fully convolutional, it can take almost any input size
+        # Furthermore we don't want any augmentation transformation on the inference
+        self.transform_test = torchvision.transforms.Compose([
+            torchvision.transforms.Resize(size),
+            torchvision.transforms.CenterCrop(size),
+            torchvision.transforms.ToTensor(),
+        ])
+    
+        self.dir = _dir
+
+        # list all the image pathes
+        self.paths = os.listdir(self.dir)
+
+    def __getitem__(self, index, train : bool = True):
+        path = self.paths[index]
+        # Try because sometime there is error on image reading 
+        # but we don't want the training pipeline to break
+        try:
+            img = Image.open(os.path.join(self.dir, path)).convert("RGB")
+        except:
+            return self[index + 1]
+        if train:
+            return self.transform(img)
+        else:
+            return self.transform_test(img)
+
+    def __len__(self):
+        return len(self.paths)
