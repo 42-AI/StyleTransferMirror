3a01d023a174c28f0da14208913c86fcf8fccb15
quentin feuillade montixi <quentinfeuillademontixi42@gmail.com>
2021-08-31 09:04:47 +0200

    finetune the model

--- download_utils.sh
+++ download_utils.sh
@@ -1,4 +1,4 @@
-mkdir style content loss_save samples
+mkdir style content loss_save samples custom_style
 
 wget http://web.fsktm.um.edu.my/~cschan/source/ICIP2017/wikiart.zip
 wget http://images.cocodataset.org/zips/train2017.zip
