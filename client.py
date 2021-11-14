082d1d4d9c25cf5ff593e7b9bca286ef7f2b5b0c
quentin feuillade montixi <quentinfeuillademontixi42@gmail.com>
2021-11-14 13:04:19 +0100

    server client working

--- client.py
+++ client.py
@@ -5,7 +5,12 @@ import torch
 import torch.distributed as dist
 import os
 
-FPS = 16
+from torch.utils import data
+from utils import FlatFolderDataset
+from PIL import Image
+import numpy as np
+
+FPS = 8
 BATCH_PER_SEC = 2
 
 class VideoGet:
@@ -57,6 +62,8 @@ class VideoProcess:
         self.stopped = False
         self.to_process = []
         self.processed = []
+        # Just for transforms
+        self.dataset = FlatFolderDataset("custom_style/")
 
 
     def start(self):    
@@ -66,12 +73,15 @@ class VideoProcess:
     def process(self):
         os.environ['MASTER_ADDR'] = '127.0.0.1'
         os.environ['MASTER_PORT'] = '29500'
-        dist.init_process_group('gloo', rank=1, world_size=2)
+        dist.init_process_group('gloo', rank=0, world_size=2)
         while not self.stopped:
             if len(self.to_process):
                 # Do process here
-                _tensor = torch.tensor(self.to_process[0])
-                self.processed.append(_tensor.detach().numpy())
+                _tensor = torch.cat([torch.unsqueeze(self.dataset.transform_test(Image.fromarray(frame)),0) for frame in self.to_process[0]])
+                _res = torch.zeros_like(_tensor)
+                dist.send(_tensor, 1)
+                dist.recv(_res, 1)
+                self.processed.append([np.moveaxis(frame.numpy(), (0, 2, 1), (2, 1, 0)) for frame in _res])
                 self.to_process = self.to_process[1:]
 
     def stop(self):
@@ -105,6 +115,11 @@ video_getter = VideoGet(0).start()
 video_shower = VideoShow(video_getter.frame).start()
 video_processer = VideoProcess().start()
 
+reset_each = 1000
+reset_each_c = reset_each
+
+initial_wait = 3
+
 time_slide = 0
 
 while True:
@@ -113,12 +128,19 @@ while True:
         video_getter.stop()
         video_processer.stop()
         break
+    if (reset_each_c <= 0):
+        print("reset")
+        video_getter.to_process = video_getter.to_process[-1:]
+        video_processer.to_process = video_processer.to_process[-1:]
+        video_processer.processed = video_processer.processed[-1:]
+        reset_each_c = reset_each
     if (len(video_getter.to_process)):
         for tp in video_getter.to_process:
             video_processer.to_process.append(tp)
         video_getter.to_process = []
-    if (len(video_processer.processed)):
+    if (len(video_processer.processed) > initial_wait):
         a = time.time_ns()
+        initial_wait = 0
         _frames = video_processer.processed[0]
         video_processer.processed = video_processer.processed[1:]
         for frame in _frames:
@@ -126,9 +148,10 @@ while True:
                 break
             video_shower.frame = frame
             time_slide += (time.time_ns() - a) * 1e-9
-            if time_slide < 1/FPS:
-                time.sleep(1/FPS - time_slide)
+            if time_slide < 1/(FPS-2):
+                time.sleep(1/(FPS-2) - time_slide)
                 time_slide = 0
             else:
-                time_slide -= 1/FPS
+                time_slide -= 1/(FPS-2)
+            reset_each_c -= 1
             a = time.time_ns()
---
+++ server.py
@@ -0,0 +1,82 @@
+from IPython.display import clear_output
+from PIL import Image
+from matplotlib import cm
+
+import torchvision
+import numpy as np
+import torch
+from torch import nn
+from torchvision import transforms
+
+import model
+import utils
+from utils import FlatFolderDataset
+
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
+import json
+import cv2
+import os
+
+import torch.distributed as dist
+
+# This is a very useful class that load all the useful config from a json file
+class DevEnvironment():
+    def __init__(self, config_file):
+        '''
+        config_file: path to the config file
+        '''
+        config = json.loads(open(config_file).read())
+
+        # This is the device used for training
+        self.device = torch.device(config["device"]) if "device" in config else torch.device("cuda")
+
+        # Setting up the layers we need on the vgg pretrained encoder
+        self.vgg_encoder = model.vgg
+        self.vgg_encoder.load_state_dict(torch.load("./model_save/vgg_normalised.pth"))
+        self.vgg_encoder = nn.Sequential(*list(self.vgg_encoder.children())[:44])
+
+        # Setting up the image decoder
+        self.decoder = model.decoder.to(self.device)
+
+        # And then the Style Attention Network
+        self.network : model.MultiLevelStyleAttention = model.MultiLevelStyleAttention(self.vgg_encoder, self.decoder)
+        self.network.to(self.device)
+        self.network.eval()
+
+    def load_save(self, file_path : str):
+        # Loading the dict of all parameters dict from the file located at file_path
+        saved = torch.load(file_path, map_location=lambda storage, loc: storage)
+
+        # Loading the different part of the model separatly
+        self.network.decoder.load_state_dict(saved["decoder"], strict=False)
+        self.network.sa_module.load_state_dict(saved["sa_module"], strict=False)
+
+# Loading the configs in env variable
+env = DevEnvironment("config.json")
+
+# Load the preceding models if not the first training step
+start_iteration = 177000
+if start_iteration != 0:
+    env.load_save(f"model_save/{str(start_iteration).zfill(6)}.pt")
+
+# Use custom style folder to display sample data
+custom_style_dataset = FlatFolderDataset("custom_style/", size=256)
+
+print("Ready for data, please connect the client")
+os.environ['MASTER_ADDR'] = '127.0.0.1'
+os.environ['MASTER_PORT'] = '29500'
+dist.init_process_group('gloo', rank=1, world_size=2)
+
+style_len = len(custom_style_dataset)
+style_index = 0
+
+with torch.cuda.amp.autocast() and torch.no_grad():
+    style = torch.cat([torch.unsqueeze(custom_style_dataset.__getitem__(2, False), 0) for _ in range(4)]).to(env.device)
+    _tensor = torch.zeros((4, 3, 512, 512))
+    while True:
+        dist.recv(_tensor, 0)
+        _out = env.network(_tensor.to(env.device), style, train=False)
+        dist.send(_out.cpu(), 0)
